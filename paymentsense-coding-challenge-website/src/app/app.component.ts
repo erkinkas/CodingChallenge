a53463b5a3f0f4fd78cf763f48e1ec463ef9d0d3
Erkin Kasymov <erkin.kas@gmail.com>
2021-01-27 12:08:12 +0000

    cleanup

--- paymentsense-coding-challenge-website/src/app/app.component.ts
+++ paymentsense-coding-challenge-website/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { PaymentsenseCodingChallengeApiService } from './services';
-import { take } from 'rxjs/operators';
-import { faThumbsUp, faThumbsDown } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
