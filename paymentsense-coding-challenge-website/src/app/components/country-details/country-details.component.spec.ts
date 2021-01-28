18e7c93be58fae36902a813bb033beab9e329145
Erkin Kasymov <erkin.kas@gmail.com>
2021-01-28 02:37:11 +0000

    frontend tests fixed

--- paymentsense-coding-challenge-website/src/app/components/country-details/country-details.component.spec.ts
+++ paymentsense-coding-challenge-website/src/app/components/country-details/country-details.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { MaterialModule } from '../../material.module';
 
 import { CountryDetailsComponent } from './country-details.component';
 
@@ -14,6 +17,8 @@ describe('CountryDetailsComponent', () => {
         CountryDetailsComponent
       ],
       imports: [
+        MaterialModule,
+        HttpClientTestingModule,
         RouterTestingModule
       ]
     })
